ee78adfb104a92cf5834846ec3ec117a448ba366
Kejin Zhou <kejin.zhou@aalto.fi>
2020-07-16 22:48:01 +0300

    teht 1-4 tehty

--- osa1/osa-1a/src/index.js
+++ osa1/osa-1a/src/index.js
@@ -35,47 +35,28 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header   name     = {course} />
-      <Content  list   = {[part1, part2, part3]} />
-      <Total    list   = {[part1, part2, part3]} />
+      <Content  list     = {parts}  />
+      <Total    list     = {parts}  />
     </div>
   )
 }
-// const App = () => {
-//   const course = 'Half Stack application development'
-//   const part1 = 'Fundamentals of React'
-//   const exercises1 = 10
-//   const part2 = 'Using props to pass data'
-//   const exercises2 = 7
-//   const part3 = 'State of a component'
-//   const exercises3 = 14
-//
-//   return (
-//     <div>
-//       <Header name = {course}/>
-//       <Content name = {[part1, part2, part3]} value = {[exercises1,exercises2,exercises3]}/>
-//       <Total value = {exercises1 + exercises2 + exercises3}/>
-//     </div>
-//   )
-//   // <Content names = {[part1, part2, part3]} values = [exercises1, exercises2, exercises3] />
-//   // <Content name = {part2} value = {exercises2}/>
-//   // <Content name = {part3} value = {exercises3}/>
-// }
 
 ReactDOM.render(<App />, document.getElementById('root'))
