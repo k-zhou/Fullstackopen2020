235f58e777ce1578032bfb6af6292a6a0799c142
Kejin Zhou <kejin.zhou@aalto.fi>
2020-07-16 22:56:12 +0300

    teht 1-5 tehty

--- osa1/osa-1a/src/index.js
+++ osa1/osa-1a/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 const Header = (props) => {
   return (
     <div>
-      <h1>{props.name}</h1>
+      <h1>{props.course.name}</h1>
     </div>
   )
 }
@@ -17,44 +17,40 @@ const Part = (props) => {
 const Content = (props) => {
   let output = []
   /* note: the scope of a "let" variable is the BLOCK it's defined in. The scope of "var" is the entire function. */
-  props.list.forEach(obj => output.push(<Part part = {obj}/> ))  //DEBUG
+  props.course.parts.forEach(obj => output.push(<Part part = {obj}/> ))  //DEBUG
   return <div>{output}</div>
 }
 
 const Total = (props) => {
   let sum = 0
-  props.list.forEach(obj => sum += obj.exercises)
-  return (
-    <div>
-      <p>
-        Number of exercises {sum}
-      </p>
-    </div>
-  )
+  props.course.parts.forEach(obj => sum += obj.exercises)
+  return <p>Number of exercises {sum}</p>
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header   name     = {course} />
-      <Content  list     = {parts}  />
-      <Total    list     = {parts}  />
+      <Header   course = {course}/>
+      <Content  course = {course}/>
+      <Total    course = {course}/>
     </div>
   )
 }
