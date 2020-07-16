c65382cb20cdd6d463ee00480c19316c5cad4467
Kejin Zhou <kejin.zhou@aalto.fi>
2020-07-16 22:44:53 +0300

    teht 1-3 tehty

--- osa1/osa-1a/src/index.js
+++ osa1/osa-1a/src/index.js
@@ -10,68 +10,72 @@ const Header = (props) => {
 }
 
 const Part = (props) => {
-  return (
-    <div>
-      <p>
-        {props.name} {props.value}
-      </p>
-    </div>
-  )
+  // console.log("name: " + props.part.name + " ex: " + props.part.exercises ) //DEBUG
+  return <p>{props.part.name} {props.part.exercises}</p>
 }
 
-// tuli kokeiltua asioita ta'ssa', en tied' onko hyva'ntapaista koodia
 const Content = (props) => {
-  var output = [];
-  for (var i = 0; i < props.name.length; ++i) {
-    output = output.concat(<p>{props.name[i]} {props.value[i]}</p>)
-  }
-  return (
-    <div>
-      {output}
-    </div>
-  )
-  // output = output.concat(<p>It: {i}</p>) //DEBUG
-  // output = output.concat(<p>This is another line.</p>)
-  // output = output.concat(<p>This is yet another line.</p>)
-  // return (
-  //   <div>
-  //     <Part name = "test1" value = {1}/>
-  //     <Part name = "test2" value = {2}/>
-  //     <Part name = "test3" value = {3}/>
-  //   </div>
-  // )
+  let output = []
+  /* note: the scope of a "let" variable is the BLOCK it's defined in. The scope of "var" is the entire function. */
+  props.list.forEach(obj => output.push(<Part part = {obj}/> ))  //DEBUG
+  return <div>{output}</div>
 }
 
 const Total = (props) => {
+  let sum = 0
+  props.list.forEach(obj => sum += obj.exercises)
   return (
     <div>
       <p>
-        Number of exercises {props.value}
+        Number of exercises {sum}
       </p>
     </div>
   )
 }
 
-
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
 
   return (
     <div>
-      <Header name = {course}/>
-      <Content name = {[part1, part2, part3]} value = {[exercises1,exercises2,exercises3]}/>
-      <Total value = {exercises1 + exercises2 + exercises3}/>
+      <Header   name     = {course} />
+      <Content  list   = {[part1, part2, part3]} />
+      <Total    list   = {[part1, part2, part3]} />
     </div>
   )
-  // <Content names = {[part1, part2, part3]} values = [exercises1, exercises2, exercises3] />
-  // <Content name = {part2} value = {exercises2}/>
-  // <Content name = {part3} value = {exercises3}/>
 }
+// const App = () => {
+//   const course = 'Half Stack application development'
+//   const part1 = 'Fundamentals of React'
+//   const exercises1 = 10
+//   const part2 = 'Using props to pass data'
+//   const exercises2 = 7
+//   const part3 = 'State of a component'
+//   const exercises3 = 14
+//
+//   return (
+//     <div>
+//       <Header name = {course}/>
+//       <Content name = {[part1, part2, part3]} value = {[exercises1,exercises2,exercises3]}/>
+//       <Total value = {exercises1 + exercises2 + exercises3}/>
+//     </div>
+//   )
+//   // <Content names = {[part1, part2, part3]} values = [exercises1, exercises2, exercises3] />
+//   // <Content name = {part2} value = {exercises2}/>
+//   // <Content name = {part3} value = {exercises3}/>
+// }
 
 ReactDOM.render(<App />, document.getElementById('root'))
