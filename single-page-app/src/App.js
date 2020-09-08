8c02ecec7e587b2b6f0ae235ae8a6c31180ff3cd
Kejin Zhou <kejin.zhou@aalto.fi>
2020-09-08 22:26:36 +0300

    teht 2.11

--- single-page-app/src/App.js
+++ single-page-app/src/App.js
@@ -1,18 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import FilterForm from './FilterForm'
 import NameForm from './NameForm'
 import DisplayNames from './DisplayNames'
-
-
-
+/**/
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+
+  const [persons, setPersons] = useState([])
   const [peopleFilter, setPeopleFilter] = useState('')
+  useEffect( () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then( resp => {
+          setPersons(resp.data)
+      })
+  }, [])  // note to self: remember to add the empty array, else it will endlessly call GET
 
   return (
     <div>
