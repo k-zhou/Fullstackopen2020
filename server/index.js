472f21575babae1bc32ac9473079810c7a9eb751
Kejin Zhou <kejin.zhou@aalto.fi>
2020-09-26 00:59:14 +0300

    3.6 finished

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ let orig_data_h_ = [
     number: "39-23-6423122"
   }
 ]
-let people_h_ = orig_data_h_.slice()
+let people_ = orig_data_h_.slice()
 
 // functions
 const getRandomInt = (max) => {
@@ -46,14 +46,14 @@ app.get('/', (req, res) => {
 
 // People pages
 app.get('/api/persons/', (req, res) => {
-  res.json(people_h_)
+  res.json(people_)
 })
 
 app.get('/api/persons/:id', (req, res) => {
   const id_ = Number(req.params.id)
   // console.log("id is", id_ ) // DEBUG
-  // people_h_.map(p => console.log(p.id, p.name, p.number)) // DEBUG
-  const found = people_h_.find(p => p.id === id_ )
+  // people_.map(p => console.log(p.id, p.name, p.number)) // DEBUG
+  const found = people_.find(p => p.id === id_ )
   if (found)
     res.json(found)
   else
@@ -64,37 +64,51 @@ app.get('/api/persons/:id', (req, res) => {
 app.get('/info', (req, res) => {
   const output = `
     <h1>Info</h1>
-    <p>There are currently ${people_h_.length} people in the phonebook.<p>
+    <p>There are currently ${people_.length} people in the phonebook.<p>
     <p>${new Date()}</p>
   `
   res.send(output)
 })
 
-// Add a new person, does not check pre-existing people yet
+// Add a new person
 app.post('/api/persons/', (req, res) => {
   // find unused id
   const MAX_RANDOM = 1000000
   let new_id_   = getRandomInt(MAX_RANDOM)
-  while ( people_h_.filter( p => p.id === new_id_ ).length > 0 )  // untested for falsies and truthies
+  while ( people_.filter( p => p.id === new_id_ ).length > 0 )  // untested for falsies and truthies
     new_id_     = getRandomInt(MAX_RANDOM)
   
   let new_note_ = req.body
   console.log("Received new POST request:", req.headers["content-type"], "- assigned", new_id_, "\n", new_note_) // DEBUG
   if (req.headers["content-type"] === "application/json") {
+    // Data checks
+    if (!new_note_.name) {
+      res.status(404).end('Needs a name for the person.') 
+      return
+    }
+    if (!new_note_.number){
+      res.status(404).end('Please specify a number.')
+      return
+    }
+    if (people_.find( p => p.name === new_note_.name)){
+      res.status(404).end(`${new_note_.name} already exists`)
+      return
+    }
+    // Add new data
     new_note_.id  = new_id_
-    people_h_ = people_h_.concat(new_note_)
+    people_ = people_.concat(new_note_)
     res.status(200).end()
   } 
   else {
-    res.status(404).end()
+    res.status(404).end('Cannot handle input.')
   }
 })
 
 // Remove a person, handles non-existent entries
 app.delete('/api/persons/:id', (req, res) => {
   const id_ = Number(req.params.id)
-  if (people_h_.find(p => p.id === id_)) {
-    people_h_ = people_h_.filter( p => p.id !== id_ )
+  if (people_.find(p => p.id === id_)) {
+    people_ = people_.filter( p => p.id !== id_ )
     res.status(204).end()
   } else {
     res.status(404).end()
