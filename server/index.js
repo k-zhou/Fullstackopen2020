645ee0655b7b8025222258f9170b4f31eb9de671
Kejin Zhou <kejin.zhou@aalto.fi>
2020-09-26 00:36:22 +0300

    3.5 finished

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const express = require('express')
 
 // Init
 const app = express()
+app.use(express.json())
 let orig_data_h_ = [
   {
     id: 1,
@@ -33,6 +34,11 @@ let orig_data_h_ = [
 ]
 let people_h_ = orig_data_h_.slice()
 
+// functions
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 // Landing page
 app.get('/', (req, res) => {
   res.send('<h1>Hello world!</h1><p>This is a simple server.</p><p>It has people data in it.</p>')
@@ -54,7 +60,7 @@ app.get('/api/persons/:id', (req, res) => {
     res.status(404).end('<h1>404 Person not found</h1>')
 })
 
-//
+// General info
 app.get('/info', (req, res) => {
   const output = `
     <h1>Info</h1>
@@ -64,7 +70,27 @@ app.get('/info', (req, res) => {
   res.send(output)
 })
 
-//
+// Add a new person, does not check pre-existing people yet
+app.post('/api/persons/', (req, res) => {
+  // find unused id
+  const MAX_RANDOM = 1000000
+  let new_id_   = getRandomInt(MAX_RANDOM)
+  while ( people_h_.filter( p => p.id === new_id_ ).length > 0 )  // untested for falsies and truthies
+    new_id_     = getRandomInt(MAX_RANDOM)
+  
+  let new_note_ = req.body
+  console.log("Received new POST request:", req.headers["content-type"], "- assigned", new_id_, "\n", new_note_) // DEBUG
+  if (req.headers["content-type"] === "application/json") {
+    new_note_.id  = new_id_
+    people_h_ = people_h_.concat(new_note_)
+    res.status(200).end()
+  } 
+  else {
+    res.status(404).end()
+  }
+})
+
+// Remove a person, handles non-existent entries
 app.delete('/api/persons/:id', (req, res) => {
   const id_ = Number(req.params.id)
   if (people_h_.find(p => p.id === id_)) {
@@ -74,6 +100,7 @@ app.delete('/api/persons/:id', (req, res) => {
     res.status(404).end()
   }
 })
+
 // Run server
 const port = 3001
 app.listen(port)
